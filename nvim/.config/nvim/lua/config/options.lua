c65cd9c1e427a112abe494fa1c00ef2943710369
d-tecnolife <dtecnolife@proton.me>
2026-02-08 19:13:16 -0600

    update

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,3 +3,5 @@
 -- Add any additional options here
 
 vim.opt.clipboard = "unnamedplus"
+vim.opt.wrap = true
+vim.opt.linebreak = true
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
   "bluz71/vim-nightfly-colors",
   name = "nightfly",
+  lazy = false,
   priority = 1000,
   config = function()
     vim.cmd("colorscheme nightfly")
+
+    -- Fix race condition: re-apply TS highlights after full startup
+    vim.api.nvim_create_autocmd("VimEnter", {
+      callback = function()
+        vim.schedule(function()
+          vim.cmd("doautocmd ColorScheme")
+        end)
+      end,
+    })
 
     -- make background transparent
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })

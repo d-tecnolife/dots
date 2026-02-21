da10f5d87eacebfeb3ee178c37c83457b8103d94
d-tecnolife <dtecnolife@proton.me>
2026-02-21 08:36:01 -0600

    update

--- fish/.config/fish/functions/ytm.fish
+++ fish/.config/fish/functions/ytm.fish
@@ -2,4 +2,5 @@ function ytm -d "Download audio from YouTube as Opus"
     yt-dlp -f 'ba[acodec=opus]/ba' -x --audio-format opus --audio-quality 0 \
         --embed-metadata \
         -o ~/music/'%(title)s.%(ext)s' $argv
+    and beet import ~/music
 end

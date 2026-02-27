592db53b85387961f4583fa8afe0efd33104a131
d-tecnolife <dtecnolife@proton.me>
2026-02-27 11:18:17 -0600

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,4 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-alias gmail 'firefoxpwa site launch 01KHXTNDCYKECRDMKHB82FAP9N'
-alias protonmail 'firefoxpwa site launch 01KHY0ZPBRNN122TXD38RSMH87'
-alias discord 'firefoxpwa site launch 01KHXYK4TNWE0TJE8QPJ6A9P3T'
+fish_add_path -g ~/.cargo/bin
